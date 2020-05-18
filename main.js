67bb66af10205373d71eea2e3afd34904d10d424
Liz <34827490+lis281@users.noreply.github.com>
2020-05-17 18:17:23 -0700

    smooth scrolling, timeline

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 $(document).ready(function () {
-  $('.menu-toggler').on('click', function ()  {
-    $(this).toggleClass('open');
-    $('.top-nav').toggleClass('open');
+
+  // $('.menu-toggler').on('click', function ()  {
+  //   $(this).toggleClass('open');
+  //   $('.top-nav').toggleClass('open');
+  // });
+
+  $('nav a[href*="#"]').on('click', function() {
+    $('html,body').animate({
+      scrollTop: $($(this).attr('href')).offset().top - 100
+    }, 500);
   });
+
+  // $('#up').on('click', function() {
+  //   ${'html', 'body'}.animate({
+  //     scrollTop: 0
+  //   }, options: 2000);
+  // });
+  $('#up').on("click", function () {
+    $("html,body").animate({
+      scrollTop: 0
+   }, 1000);
+ });
+
+  AOS.init({
+    easing: 'ease',
+    duration: 1500,
+    once: true
+  })
 });
