8250fbf4b54fa41b605d6627a6800e28e96b20ca
Liz <34827490+lis281@users.noreply.github.com>
2020-06-13 18:53:37 -0700

    updated courses page

--- main.js
+++ main.js
@@ -27,4 +27,14 @@ $(document).ready(function () {
     duration: 1500,
     once: true
   })
+
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 });
+
