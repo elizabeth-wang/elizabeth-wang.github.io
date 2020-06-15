8071c42ab3cbfa7d66e7cbabfac47674cc80fa99
Liz <34827490+lis281@users.noreply.github.com>
2020-06-15 11:05:52 -0700

    hamburger menu for mobile

--- main.js
+++ main.js
@@ -28,13 +28,6 @@ $(document).ready(function () {
     once: true
   })
 
-  function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+
 });
 
